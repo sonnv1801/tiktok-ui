23591f906ab41057fd20fa4dd9802b974ba4ba53
sonnv1801 <nguyenvanson18102001@gmail.com>
2024-02-28 09:07:51 +0700

    Bài 95 (2024)

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,15 +1,19 @@
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 import Header from '~/components/Layout/components/Header';
 import Sidebar from './Sidebar';
 
+const cx = classNames.bind(styles);
+
 function DefaultLayout({ children }) {
     return (
-        <>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
-        </>
+        </div>
     );
 }
 
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
 function Header() {
-    return <h1>Header Page</h1>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>ok</div>
+        </header>
+    );
 }
 
 export default Header;
